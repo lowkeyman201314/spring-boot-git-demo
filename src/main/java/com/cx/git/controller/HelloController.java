7d8326cb6fc185f8332bbb62d63601a272955521
laoyu <480337728@qq.com>
2021-09-08 11:46:51 +0800

    提交整个项目

---
+++ src/main/java/com/cx/git/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.cx.git.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author ThinkPad
+ * @CreationDate 2021/9/8 11:10
+ * @Description 。。。
+ */
+@RestController
+public class HelloController {
+}
---
+++ src/test/java/com/cx/git/SpringBootGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cx.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
